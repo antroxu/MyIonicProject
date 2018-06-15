c4c5fb769909a3603ffa83951b417ca9745b6b0e
antroxu <antroxu2011@gmail.com>
2018-06-15 10:05:19 +0200

    pequeños cambios

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -3,7 +3,7 @@ import { Component, style } from '@angular/core';
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html',
-  styles: [`p { font-size: 40px}`]
+  // styles: [`p { font-size: 40px}`]
 })
 export class HelloIonicPage {
   constructor() {
