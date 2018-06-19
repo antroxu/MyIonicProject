455f69dbb64b67d2dec55eddb9909b9592af7a49
Dev1 <Dev1@GET12-04.local>
2018-06-19 11:28:44 +0200

    servicio post para login

--- src/pages/formulario/formulario.ts
+++ src/pages/formulario/formulario.ts
@@ -1,16 +1,20 @@
 import { Component, style } from '@angular/core';
 import { Login } from '../../app/login.model';
+import { LoginService } from '../../app/login.service';
+import { Observable } from 'rxjs/Observable';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'formulario',
   templateUrl: 'formulario.html',
   // styles: [`p { font-size: 40px}`]
+  providers: [LoginService]
 })
 export class FormularioComponent {
 
   login: Login;
   
-  constructor() {
+  constructor(private loginservice : LoginService) {
     this.login = new Login();
 
   }
@@ -20,6 +24,25 @@ export class FormularioComponent {
     if (valido){
       //Procesa datos
       console.log ("Los datos son válidos");
+      let resp : Observable<object> = this.loginservice.postLogin(datos);
+      resp.subscribe(
+        ok => {
+                console.log ("HA VUELTO");
+                let vresp : HttpResponse<Object> = <HttpResponse<Object>>ok;                
+                console.log ("CUERPO: " + vresp.body.saludo);
+                console.log ("STATUS: " + vresp.status);
+
+              },
+        ko => {
+                console.log ("Respiesta ERROR " + ko);
+                let vresp : HttpResponse<Object> = <HttpResponse<Object>>ko;                
+                console.log ("STATUS: " + vresp.status);
+
+              },
+        () => {
+                console.log ("COMPLETADO"); 
+              }
+      )
     }
     else {
       //Error
